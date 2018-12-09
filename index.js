ec36c225e11876ea17f752e15c8cfa895cd52092
Dominic <mrhid6@gmail.com>
2018-12-09 01:18:50 +0000

    added cleanup to utils

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const Mrhid6Utils = {}
 Mrhid6Utils.NET = {}
 Mrhid6Utils.Config = require("./lib/config.js");
 Mrhid6Utils.DatabaseHelper = require("./lib/databasehelper");
+Mrhid6Utils.Cleanup = require("./lib/cleanup");
 
 module.exports = Mrhid6Utils;
