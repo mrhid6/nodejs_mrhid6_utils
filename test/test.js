6da16d15a2c1e4faa874fcbf01740629dc67cdf9
Dominic <mrhid6@gmail.com>
2019-04-09 22:14:22 +0100

    config issues

--- test/test.js
+++ test/test.js
@@ -1,29 +1,17 @@
 const Mrhid6Utils = require("../index.js");
 
 const configOptions = {
-    configFileName: "./test_config.json"
+    configFileName: "./test_config.json",
+    defaultConfigFile: "./default_config.json"
 }
 
-const config = new Mrhid6Utils.Config(configOptions);
-const DB = new Mrhid6Utils.DatabaseHelperNew(config);
-const logger = new Mrhid6Utils.Logger(config, {
-    basedir: __dirname + "/../"
-});
-
-DB.createConnection();
 
 
-DB.testConnection().then(() => {
-    console.log("connected!");
-}, () => {
-    console.log("not Connected!");
-});
-let count = 0;
-for (let i = 0; i < 10000; i++) {
-    DB.queryUsingSQLFile(__dirname + "/../test/players.sql").then(rows => {
-        count++;
-        console.log(count);
-    }).catch(err => {
-        console.log(err);
-    })
+
+for (let i = 0; i < 1; i++) {
+    const config = new Mrhid6Utils.Config(configOptions);
+    if (config._data.mysql.host == "TEST") {
+        console.log(i);
+        break;
+    }
 }
