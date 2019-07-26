f1f4b834ab2f4a603f1505523157b2bf9c1291c3
Dominic <mrhid6@gmail.com>
2019-07-26 22:59:54 +0100

    test connection once connected

--- test/test.js
+++ test/test.js
@@ -9,16 +9,8 @@ const config = new Mrhid6Utils.Config(configOptions);
 
 const DB = new Mrhid6Utils.DatabaseHelperNew(config);
 
-DB.createConnection()
-
-DB.testConnection().then(() => {
+DB.createConnection().then(() => {
     console.log("connected!");
-
-    for (var i = 0; i < 400; i++) {
-        DB.query("SHOW TABLES").then(() => {
-            console.log("1");
-        })
-    }
 
 }).catch(err => {
     console.log("testConnection failed!");
