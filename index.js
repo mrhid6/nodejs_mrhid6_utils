1364e8600e5336e02d1ed54754cd47da12c8f4a0
Mrhid6 <mrhid6@gmail.com>
2020-02-05 10:38:41 +0000

    fixes for config and databasehelper

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ Mrhid6Utils.NET = {}
 Mrhid6Utils.Config = require("./lib/config.js");
 Mrhid6Utils.FileCache = require("./lib/filecache.js");
 Mrhid6Utils.DatabaseHelper = require("./lib/databasehelper");
-Mrhid6Utils.DatabaseHelperNew = require("./lib/databasehelper_new");
 Mrhid6Utils.Cleanup = require("./lib/cleanup");
 Mrhid6Utils.Logger = require("./lib/logger");
 Mrhid6Utils.RedisHelper = require("./lib/redishelper");
