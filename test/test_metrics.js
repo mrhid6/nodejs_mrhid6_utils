877146b2eac45116dd2320366fcc5c0385ea5444
Mrhid6 <mrhid6@gmail.com>
2020-02-05 19:36:23 +0000

    more work on metrics

--- test/test_metrics.js
+++ test/test_metrics.js
@@ -1,7 +1,9 @@
 const Mrhid6Utils = require("../");
 const Metrics = new Mrhid6Utils.Metrics({
-    appid: "APP-189WI4M396JD72"
-    
+    appid: "APP-189WI4M396JD72",
+    client_uuid: "Blt-nE5SZn1-HIl2"
 })
+
+Metrics.createEvent(1, "Test", 1)
 
 console.log(Metrics)
