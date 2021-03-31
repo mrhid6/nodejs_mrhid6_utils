2730ea694bd978b322dafc482f7ae45d6f9372eb
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2021-03-31 15:42:53 +0100

    added basedir to dbhelper

--- test/test.js
+++ test/test.js
@@ -1,22 +1,2 @@
 const Mrhid6Utils = require("../index.js");
 
-const options = {
-    serialize_data: true
-}
-
-const RedisHelper = new Mrhid6Utils.RedisHelper(options);
-
-
-RedisHelper.checkDataInRedis("Test").then(check_result => {
-    if (check_result == false) {
-        RedisHelper.saveDataInRedis("Test", "hello!").then(() => {
-            console.log("Saved!")
-        })
-    } else {
-        RedisHelper.getDataFromRedis("Test").then(data => {
-            console.log(data)
-        });
-    }
-}).catch(err => {
-    console.log(err);
-});
