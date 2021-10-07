77eeaf6020dd8bf866a7754ea4d3f263f25a635b
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2021-10-07 19:32:39 +0100

    package upgrade

--- test/test_mysqlmodel.js
+++ test/test_mysqlmodel.js
@@ -1,8 +1,13 @@
-
 const iModel = require("../").MySQLModel;
 
-const ModelTest = new iModel(null, {translations:[["ci_pclevel", "PowerCoreLevel"]]});
-const testData={
-"ci_pclevel": 1
-}
+const ModelTest = new iModel(null, {
+    translations: [
+        ["ci_pclevel", "PowerCoreLevel"]
+    ]
+});
+const testData = [{
+    "ci_pclevel": 1
+}, {
+    "ci_pclevel": 2
+}]
 ModelTest.ParseMySQLData(testData);
