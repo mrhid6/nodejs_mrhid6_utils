7bc01177c32b08632192e45242f2e35a711cf606
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2022-07-12 12:20:28 +0000

    updated logger

--- test/test.js
+++ test/test.js
@@ -1,35 +1,16 @@
 const Mrhid6Utils = require("../index.js");
 
-const iUserModel = require("./test_mysqlmodel");
+const Logger = Mrhid6Utils.Logger;
 
-const Config = new Mrhid6Utils.Config({
-    configPath: __dirname,
-    filename: "test_config.json"
-});
 
-Config.load().then(() => {
-    const MysqlHelper = new Mrhid6Utils.DB.MySQL(Config);
-
-    MysqlHelper.init().then(() => {
-        MysqlHelper.setScriptsDirectory(__dirname)
-        const uModel = new iUserModel(MysqlHelper);
-        uModel.fromJson({
-            UserId: 3
-        });
-
-        uModel.retrieve().then(() => {
-
-            //console.log(uModel.toJson());
-
-            const tempModel = new iUserModel(MysqlHelper);
-            tempModel.fromJson(uModel.toJson());
-            console.log(tempModel.toJson());
+class MainLogger extends Logger {
+    constructor() {
+        super({
+            logName: "Testing"
         })
+    }
+}
 
+const theLogger = new MainLogger();
 
-
-
-    }).catch(err => {
-        console.log(err);
-    })
-})
+theLogger.init();
