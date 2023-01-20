fce6edc573b43751614415153abc367034749bbd
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2023-01-20 11:55:02 +0000

    feat: Added variable cache

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const Mrhid6Utils = {}
+const Mrhid6Utils = {};
 
-Mrhid6Utils.NET = {}
+Mrhid6Utils.NET = {};
 Mrhid6Utils.Config = require("./lib/config.js");
 Mrhid6Utils.FileCache = require("./lib/filecache.js");
 Mrhid6Utils.DB = {
     QueryTypes: require("./lib/DB/databasehelper").QueryTypes,
     MySQL: require("./lib/DB/db_helper_mysql"),
-    SQLite: require("./lib/DB/db_helper_sqlite")
-}
+    SQLite: require("./lib/DB/db_helper_sqlite"),
+};
 Mrhid6Utils.DatabaseHelper = require("./lib/DB/db_helper_mysql");
 Mrhid6Utils.Cleanup = require("./lib/cleanup");
 Mrhid6Utils.Logger = require("./lib/logger");
@@ -17,8 +17,8 @@ Mrhid6Utils.Models = {
     Model: require("./lib/model").Model,
     Translation: require("./lib/model").Translation,
     Link: require("./lib/model").Link,
-    Types: require("./lib/model").TYPES
-}
+    Types: require("./lib/model").TYPES,
+};
 Mrhid6Utils.NetworkServer = require("./lib/networkserver");
 Mrhid6Utils.NetworkClient = require("./lib/networkclient");
 
@@ -27,5 +27,6 @@ Mrhid6Utils.WSNetworkServer = require("./lib/WSNetworkServer");
 Mrhid6Utils.Tools = require("./lib/tools");
 
 Mrhid6Utils.DockerHelper = require("./lib/docker_helper");
+Mrhid6Utils.VarCache = require("./lib/varcache");
 
 module.exports = Mrhid6Utils;
