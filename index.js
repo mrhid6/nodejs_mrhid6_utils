e67e854d6b81a4adc757b67d214d5198f933110d
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2023-05-25 12:30:04 +0000

    feat: cleanup

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports.Config = require("./lib/config.js");
 module.exports.FileCache = require("./lib/filecache.js");
 
-module.exports.DatabaseHelper = require("./lib/DB/db_helper_mysql");
 module.exports.Cleanup = require("./lib/cleanup");
 module.exports.Logger = require("./lib/logger");
 
