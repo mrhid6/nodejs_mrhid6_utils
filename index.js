97750294920f915e846a2d9384916762a3647a91
Mrhid6 <dominic.richardson1@hotmail.co.uk>
2023-05-25 10:53:16 +0000

    feat: Removed unused files

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const Mrhid6Utils = {};
+module.exports.Config = require("./lib/config.js");
+module.exports.FileCache = require("./lib/filecache.js");
 
-Mrhid6Utils.NET = {};
-Mrhid6Utils.Config = require("./lib/config.js");
-Mrhid6Utils.FileCache = require("./lib/filecache.js");
-Mrhid6Utils.DB = {
-    QueryTypes: require("./lib/DB/databasehelper").QueryTypes,
-    MySQL: require("./lib/DB/db_helper_mysql"),
-};
-Mrhid6Utils.DatabaseHelper = require("./lib/DB/db_helper_mysql");
-Mrhid6Utils.Cleanup = require("./lib/cleanup");
-Mrhid6Utils.Logger = require("./lib/logger");
-Mrhid6Utils.RedisHelper = require("./lib/redishelper");
-Mrhid6Utils.Models = {
-    Model: require("./lib/model").Model,
-    Translation: require("./lib/model").Translation,
-    Link: require("./lib/model").Link,
-    Types: require("./lib/model").TYPES,
-};
-Mrhid6Utils.NetworkServer = require("./lib/networkserver");
-Mrhid6Utils.NetworkClient = require("./lib/networkclient");
+module.exports.DatabaseHelper = require("./lib/DB/db_helper_mysql");
+module.exports.Cleanup = require("./lib/cleanup");
+module.exports.Logger = require("./lib/logger");
 
-Mrhid6Utils.WSNetworkServer = require("./lib/WSNetworkServer");
+module.exports.NetworkServer = require("./lib/networkserver");
+module.exports.NetworkClient = require("./lib/networkclient");
+module.exports.WSNetworkServer = require("./lib/WSNetworkServer");
 
-Mrhid6Utils.Tools = require("./lib/tools");
+module.exports.Tools = require("./lib/tools");
 
-Mrhid6Utils.DockerHelper = require("./lib/docker_helper");
-Mrhid6Utils.VarCache = require("./lib/varcache");
-
-module.exports = Mrhid6Utils;
+module.exports.DockerHelper = require("./lib/docker_helper");
+module.exports.VarCache = require("./lib/varcache");
